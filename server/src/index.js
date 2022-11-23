dbb3c4384a4c3208afb441ff13005f7f777da41d
Qrare <Metbezu@gmail.com>
2022-11-23 22:23:36 +0300

    connected to database

--- server/src/index.js
+++ server/src/index.js
@@ -2,18 +2,18 @@ const PORT = 5000
 const Application = require('./Application')
 const app = new Application()
 const jsonParser = require('./middlewares/jsonParse')
-const sqlite3 = require('sqlite3').verbose()
+const urlParser = require('./middlewares/parseURL')
 const userRouter = require('./routes/userRoutes')
+const sqlite3 = require('sqlite3').verbose()
 
 const db = new sqlite3.Database('./todo.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) return console.error(err.message)
 })
 
-let sql;
-// sql = 'CREATE TABLE users(id INTEGER PRIMARY KEY)'
-
-
+app.use(urlParser('http://localhost:5000'))
 app.use(jsonParser)
 app.addRouter(userRouter)
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
+
+module.exports = db
