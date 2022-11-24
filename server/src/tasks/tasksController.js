954488590b343e10c4e28b57f6f2ec73c6f82100
Qrare <Metbezu@gmail.com>
2022-11-24 21:45:52 +0300

    requests to db

--- server/src/tasks/tasksController.js
+++ server/src/tasks/tasksController.js
@@ -1,33 +1,93 @@
-async function getAllTasks(req, res) {
-    try {
+const sqlite3 = require('sqlite3').verbose()
 
+const db = new sqlite3.Database('./todo.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message)
+})
+
+let sql;
+
+function getTasks(req, res) {
+    if (req.params.date) {
+    sql = 'SELECT * from tasks WHERE date = ?'
+        try {
+            db.get(sql, [req.params.date], (err, row) => {
+                if (err) return res.send({message: err})
+                res.send({data: row})
+            })
+        } catch(e) {
+            console.log(e)
+        }
+    }
+    sql = 'SELECT * FROM tasks';
+    try {
+        db.all(sql, [], (err, rows) => {
+            if (err) return res.send({message: err});
+
+            if (rows.length < 1)
+                return res.send({message: "No match"})
+            return res.send({data: rows}) //todo try just rows
+        })
     } catch(e) {
         console.log(e)
     }
 }
 
-async function getTaskById(req, res) {
+function createTask(req, res) {
     try {
-
+        const {id, content, done, date, userId} = req.body
+        sql = 'INSERT INTO tasks(id, content, done, date, userId) VALUES(?, ?, ?, ?, ?)'
+        db.run(sql, [id, content, done, date, userId], (err) => {
+            if (err) return res.send({error: err})
+        })
+        res.send({message: 'Task was created'})
     } catch(e) {
         console.log(e)
     }
 }
 
-async function getAllTasks(req, res) {
+function deleteTask(req, res) {
     try {
-
+        sql = 'DELETE from tasks WHERE id = ?'
+        db.run(sql, [req.params.id], (err) => {
+            if (err) return res.send({message: err})
+        })
+        res.send({message: "Task was deleted"})
     } catch(e) {
         console.log(e)
     }
 }
 
-async function getAllTasks(req, res) {
+function updateTask(req, res) {
     try {
-
-    } catch(e) {
+        sql = 'UPDATE tasks SET done = ? WHERE id = ?'
+        db.run(sql, [req.params.done, req.params.id], (err) => {
+            if (err) return res.send({message: err})
+        })
+        res.send({message: "Task was updated"})
+    } catch (e) {
         console.log(e)
     }
 }
 
 
+function getTasksByDateAndUser(req, res) {
+    try {
+        sql = 'SELECT * FROM tasks WHERE date = ? AND userId = ?'
+        db.all(sql, [req.params.date, req.params.userId], (err, rows) => {
+            if(err)  return res.send({message: 'error'})
+            res.send(rows)
+        })
+    } catch(e) {
+
+    }
+}
+
+module.exports = {
+    getTasks,
+    createTask,
+    deleteTask,
+    updateTask,
+    getTasksByDateAndUser
+}
+
+//todo move database logic to services
--- server/src/users/usersController.js
+++ server/src/users/usersController.js
@@ -6,20 +6,18 @@ const db = new sqlite3.Database('./todo.db', sqlite3.OPEN_READWRITE, (err) => {
 
 let sql;
 
-let users = [
-    {
-        id: 1,
-        name: 'dima'
-    },
-    {
-        id: 2,
-        name: 'sasha'
-    },
-]
 
-function  getAllUsers(req, res) {
+function  getAllUsers(req, res) { //todo Try it out
     if (req.params.id) {
-        return res.send(users.filter(user => user.id == req.params.id))
+        try {
+            sql = 'SELECT * from users WHERE id = ?'
+            db.get(sql, [req.params.id], (err, row) => {
+                if (err) return res.send({message: err})
+                res.send({data: row})
+            })
+        } catch(e) {
+            console.log(e)
+        }
     }
     sql = 'SELECT * FROM users';
     try {
@@ -51,8 +49,15 @@ function  createUser(req, res) {
 }
 
 function deleteUser(req, res) {
-    users = users.filter(user => user.id != req.params.id)
-    res.send(users)
+    try {
+        sql = 'DELETE from users WHERE id = ?'
+        db.run(sql, [req.params.id], (err) => {
+            if (err) return res.send({message: err})
+        })
+        res.send({message: "User was deleted"})
+    } catch(e) {
+        console.log(e)
+    }
 }
 
 
