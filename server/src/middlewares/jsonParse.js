1fa7e6b20f12ec5eecd013dfe0e241ad90041efd
Qrare <Metbezu@gmail.com>
2022-11-24 21:33:38 +0300

    added headers

--- server/src/middlewares/jsonParse.js
+++ server/src/middlewares/jsonParse.js
@@ -1,8 +1,11 @@
 module.exports = (req, res) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Request-Method', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Access-Control-Expose-Headers', 'Access-Control-Allow-Origin');
     res.send = (data) => {
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
-        })
         res.end(JSON.stringify(data))
     }
 }
